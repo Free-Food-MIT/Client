abfc5e0f839c644d58c1b0ba5b8a79f54b694a4b
Artem Laptiev <arlaptiev@gmail.com>
2021-09-19 08:27:15 -0400

    date-change

--- src/components/FoodCard.js
+++ src/components/FoodCard.js
@@ -4,18 +4,30 @@ import { EyeFilled, WarningOutlined } from '@ant-design/icons'
 import moment from 'moment'
 import PropTypes from 'prop-types'
 
+/* Helper function to determine the time label */
+function getTimeLabel (timestamp) {
+    let now = moment()
+    let eventTime = moment(timestamp)
+    let moreInfo = eventTime.format("M/d")
+    if (eventTime < now.add(2, "hours")) {
+        moreInfo = "right now 🔥"
+    } else if (eventTime < now.add(1, "day").startOf("day")) {
+        moreInfo = "today"
+    }
+    return `${eventTime.format("h:m a")} - ${moreInfo}`
+}
+
 function FoodCard (props) {
     return (
         <Card className='card' actions={
             [
-                <Tooltip placement="bottom" title="Report"> <WarningOutlined /> </Tooltip>,
                 <Tooltip placement="bottom" title="View"> <EyeFilled /> </Tooltip>
             ]
         }>
-            <Card.Meta title={props.title} description={`Posted by ${props.poster} ${moment(props.timestamp).from()}`} />
+            <Card.Meta title={props.title} description={getTimeLabel(props.timestamp)} />
             <br />
             <Typography.Paragraph>
-                {props.description}
+                {`By ${props.poster} @ ${props.description}`}
             </Typography.Paragraph>
         </Card>
     )
